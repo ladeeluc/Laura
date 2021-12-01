13b355a1c0600916e6f567dab037fe505ec27e7e
Ladeeluc <laura.hatch.lh@gmail.com>
2021-12-01 13:39:34 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!!!Hello World!!!");
+
+        String name = "John";
+        System.out.println(name);
+
+        int myFirstNumber = 37;
+        int mySecondNumber = (37 + 37);
+        int myThirdNumber = myFirstNumber * 12;
+        int myTotal = (myFirstNumber) + (mySecondNumber) + (myThirdNumber);
+        System.out.println(myTotal);
+        int myLastOne = (myTotal - 1000);
+        System.out.println(myLastOne);
+
+    }
+}
+
+
+
+
+
